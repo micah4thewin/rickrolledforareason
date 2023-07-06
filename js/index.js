b1591d00770a01296e6abb63ecdb43d9e9c8c7bc
Micah Levason <micah.levason@gmail.com>
2023-07-06 13:10:32 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var videoElem = document.getElementById('videoBackground');
+
+  function tryPlay() {
+    videoElem.play().then(function() {
+      // Automatic playback started!
+    }).catch(function(error) {
+      // Automatic playback failed, show a UI element to let the user manually start playback
+      videoElem.muted = true;
+      videoElem.play();
+    });
+  }
+
+  // Try to start video on page load
+  tryPlay();
+
+  // Unmute and play the video as soon as the user interacts with the page
+  document.body.addEventListener('click', function() {
+    videoElem.muted = false;
+  });
+});
